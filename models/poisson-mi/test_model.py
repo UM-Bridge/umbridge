8e9792540cb5ac3970f9b5f9ebacf8749efe8f39
Linus Seelinger <mail@linusseelinger.de>
2021-10-18 14:55:32 +0200

    Fix hostname

--- models/poisson-mi/test_model.py
+++ models/poisson-mi/test_model.py
@@ -4,7 +4,7 @@ import json
 host = 'http://model:4242/'
 
 def test_Evaluate():
-    url = f'{model}/Evaluate'
+    url = f'{host}/Evaluate'
 
     payload = {'input': [[1,0,0,0]], 'config': {}}
 
@@ -16,7 +16,7 @@ def test_Evaluate():
     assert resp_body['output'] == [[0.0671325634212171, 0.06713256342121707, 0.06713256342121707, 0.06713256342121704, 0.06713256342121704, 0.20832077985852093, 0.20832077985852088, 0.20832077985852096, 0.20832077985852082, 0.20832077985852082, 0.37308450540600907, 0.3730845054060091, 0.3730845054060091, 0.37308450540600907, 0.373084505406009, 0.5880037531004143, 0.5880037531004146, 0.5880037531004147, 0.5880037531004146, 0.5880037531004146, 0.8525808066508843, 0.8525808066508848, 0.8525808066508845, 0.8525808066508849, 0.8525808066508849], [0.012267346132887097]]
 
 def test_GetInputSizes():
-    url = f'{model}/GetInputSizes'
+    url = f'{host}/GetInputSizes'
 
     resp = requests.get(url)
     print(resp.text)
@@ -26,7 +26,7 @@ def test_GetInputSizes():
     assert resp_body['inputSizes'] == [4]
 
 def test_GetOutputSizes():
-    url = f'{model}/GetOutputSizes'
+    url = f'{host}/GetOutputSizes'
 
     resp = requests.get(url)
     print(resp.text)
