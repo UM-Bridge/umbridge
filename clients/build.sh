4307573a9de923a69a9ba152ce3471ed3edea9b3
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2021-11-23 12:57:14 +0100

    Clean up clients a bit

--- clients/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-g++ http-client.cpp -pthread -I../lib/ -I/usr/include/eigen3/ -lssl -lcrypto -o http-client
---
+++ clients/c++/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+g++ http-client.cpp -pthread -I../../lib/ -I/usr/include/eigen3/ -lssl -lcrypto -o http-client
