5b796206454cd783010c7f569c874551581526b0
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2022-01-10 17:22:17 +0100

    Set up exahype-tsunami benchmark posterior

--- benchmarks/exahype-tsunami/benchmark-server.py
+++ benchmarks/exahype-tsunami/benchmark-server.py
@@ -4,8 +4,6 @@ import scipy.stats
 class Benchmark(httpmodel.Model):
     def __init__(self, model_url):
         self.model = httpmodel.HTTPModel(model_url)
-        self.artificial_truth = [.0, .0]
-        self.artificial_data = self.model([self.artificial_truth])[0]
 
     def get_input_sizes(self):
         return self.model.get_input_sizes()
@@ -14,13 +12,22 @@ class Benchmark(httpmodel.Model):
         return [1]
 
     def __call__(self, parameters, config={}):
-        output = self.model(parameters, config)[0]
-        print(output)
-        print(config)
-        print("")
-        print(config["level"])
-        posterior = scipy.stats.multivariate_normal.logpdf(output, self.artificial_data, 1e-4)# \
-#                    + scipy.stats.multivariate_normal.logpdf(parameters[0], [0,0,0,0], 5)  # logpdf args: x, loc, scale
+        level = config["level"]
+
+        # Cut-off prior
+        if not (-239 < parameters[0][0] < 739) or not (-339 < parameters[0][1] < 339):
+            return 1234
+
+        # Likelihood definition
+        model_output = self.model(parameters, config)[0]
+
+        data = [1813.8, 0.00185232, 5278.8, 0.0006368]
+        likelihood_std_dev_time = [2.5, 1.5, 0.75]
+        likelihood_std_dev_height = [0.15, 0.1, 0.1]
+
+        likelihood_cov_matrix_diag = [likelihood_std_dev_time[level]**2, likelihood_std_dev_height[level]**2] * 2
+
+        posterior = scipy.stats.multivariate_normal.logpdf(model_output, data, likelihood_cov_matrix_diag)
         return [[posterior]]
 
 benckmark = Benchmark("http://localhost:4242")
