b15508be5121f0f2f97448ca2edccc44c43bdc8f
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2022-01-18 13:50:03 +0100

    Require c++17 for c++ client example

--- clients/c++/build.sh
+++ clients/c++/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ http-client.cpp -pthread -I../../lib/ -I/usr/include/eigen3/ -lssl -lcrypto -o http-client
+g++ -std=c++17 http-client.cpp -pthread -I../../lib/ -I/usr/include/eigen3/ -lssl -lcrypto -o http-client
