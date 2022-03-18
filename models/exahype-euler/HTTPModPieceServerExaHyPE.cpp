24e692bc4967481f42f5eb852a28b1ca5eb79c08
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2022-03-18 19:10:13 +0100

    Add builder for Euler

--- models/exahype-euler/HTTPModPieceServerExaHyPE.cpp
+++ models/exahype-euler/HTTPModPieceServerExaHyPE.cpp
@@ -32,7 +32,9 @@ public:
     inputsfile.close();
 
     int status;
-    std::string cmd = "bash -c 'cd /ExaHyPE-Tsunami/ApplicationExamples/Euler/ && source /opt/intel/oneapi/setvars.sh && mpirun -n " + std::to_string(ranks) + " ./ExaHyPE-Euler Euler_ADERDG.exahype2'";
+    //std::string cmd = "bash -c 'cd /ExaHyPE-Tsunami/ApplicationExamples/Euler/ && source /opt/intel/oneapi/setvars.sh && mpirun -n " + std::to_string(ranks) + " ./ExaHyPE-Euler Euler_ADERDG.exahype2'";
+    std::string cmd = "bash -c 'cd /ExaHyPE-Tsunami/ApplicationExamples/Euler/ && mpirun -n " + std::to_string(ranks) + " ./ExaHyPE-Euler Euler_ADERDG.exahype2'";
+    std::cout << "Executing: " << cmd << std::endl;
     status = system(cmd.c_str());
     std::cout << "Exahype exit status " << status << std::endl;
 
