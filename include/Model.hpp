00803514cec0de263414d765956c4bdb28d0d97d
Andy Davis <davisad@alum.mit.edu>
2022-03-20 14:42:34 -0400

    added include directory to copy in the docker file

---
+++ include/Model.hpp
@@ -0,0 +1,17 @@
+#ifndef UMBRIDGEMODEL_HPP_
+#define UMBRIDGEMODEL_HPP_
+
+namespace umbridge {
+
+/// A model
+class Model {
+public:
+  inline Model() {}
+
+  virtual ~Model() = default;
+private:
+};
+
+} // namespace umbridge
+
+#endif
