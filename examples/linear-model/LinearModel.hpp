69246387485c1e58a1f6a5eed0decc1403a435bb
Andy Davis <davisad@alum.mit.edu>
2022-03-20 15:12:09 -0400

    created linear model example

---
+++ examples/linear-model/LinearModel.hpp
@@ -0,0 +1,11 @@
+#ifndef UMBRIDGE_EXAMPLE_LINEARMODEL_HPP_
+#define UMBRIDGE_EXAMPLE_LINEARMODEL_HPP_
+
+#include "umbridge/Model.hpp"
+
+class LinearModel {
+public:
+private:
+};
+
+#endif
---
+++ examples/linear-model/minimal-server.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+#include "LinearModel.hpp"
+
+int main() {
+  // get the the port number
+  char const* port_cstr = std::getenv("PORT");
+  int port = 0;
+  if ( port_cstr == NULL ) {
+    std::cout << "Environment variable PORT not set! Using port 4242 as default." << std::endl;
+    port = 4242;
+  } else {
+    port = atoi(port_cstr);
+  }
+
+  std::cout << "PORT: " << port << std::endl;
+
+  std::cout << "hello world!" << std::endl;
+}
