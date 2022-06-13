d91f2fb142f517a4aee424b828140d9ecf27833a
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2022-06-13 17:35:27 +0200

    Switch c++ lib to parallel HTTP handling, instead mutex model calls

--- models/testmodel/umbridge.h
+++ models/testmodel/umbridge.h
@@ -1,10 +1,6 @@
 #ifndef UMBRIDGE
 #define UMBRIDGE
 
-// Ensure that we only have 1 handler running at once!
-// More might cause the model itself to run concurrently!
-#define CPPHTTPLIB_THREAD_POOL_COUNT 1
-
 // Increase timeout to allow for long-running models.
 // This should be (to be on the safe side) significantly greater than the maximum time your model may take
 #define CPPHTTPLIB_READ_TIMEOUT_SECOND 60*60
@@ -289,6 +285,7 @@ namespace umbridge {
   void serveModel(Model& model, std::string host, int port) {
 
     httplib::Server svr;
+    std::mutex model_mutex; // Ensure the underlying model is only called sequentially
 
     svr.Post("/Evaluate", [&](const httplib::Request &req, httplib::Response &res) {
       if (!model.SupportsEvaluate()) {
@@ -298,6 +295,8 @@ namespace umbridge {
         res.set_content(response_body.dump(), "text/plain");
         return;
       }
+
+      const std::lock_guard<std::mutex> model_lock(model_mutex);
 
       //std::cout << "Received a Get with body " << req.body << std::endl;
       json request_body = json::parse(req.body);
@@ -332,6 +331,8 @@ namespace umbridge {
         res.set_content(response_body.dump(), "text/plain");
         return;
       }
+
+      const std::lock_guard<std::mutex> model_lock(model_mutex);
 
       json request_body = json::parse(req.body);
 
@@ -369,6 +370,8 @@ namespace umbridge {
         return;
       }
 
+      const std::lock_guard<std::mutex> model_lock(model_mutex);
+
       json request_body = json::parse(req.body);
 
       unsigned int inWrt = request_body.at("inWrt");
@@ -404,6 +407,8 @@ namespace umbridge {
         res.set_content(response_body.dump(), "text/plain");
         return;
       }
+
+      const std::lock_guard<std::mutex> model_lock(model_mutex);
 
       json request_body = json::parse(req.body);
 
