20c1219a32275f2e6412cb9093a81965b02e25cc
Linus Seelinger <mail@linusseelinger.de>
2022-09-07 18:30:40 +0200

    Fix gradient in testmodel-python

--- models/testmodel-python/minimal-server.py
+++ models/testmodel-python/minimal-server.py
@@ -17,7 +17,7 @@ class TestModel(umbridge.Model):
         return True
 
     def gradient(self,out_wrt, in_wrt, parameters, sens, config={}):
-        return [2.0*parameters[0][0]*sens[0]]
+        return [2.0*sens[0]]
 
     def supports_gradient(self):
         return True
