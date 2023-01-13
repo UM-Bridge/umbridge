7f9d0f8870749fa6c0e17b3af02adb1e39f642d3
Linus Seelinger <mail@linusseelinger.de>
2023-01-13 16:46:16 +0100

    Initial MATLAB test

---
+++ testing/clients/test_matlab.m
@@ -0,0 +1,16 @@
+classdef TestUmbridge < matlab.unittest.TestCase
+
+    methods (Test)
+
+        function testConnection(testCase)
+            uri = 'http://model:4243';
+
+            model = HTTPModel(uri,'posterior');
+
+            httpValue = model.evaluate([1, 3])
+            exactValue = -11.194036030183454;
+            testCase.verifyEqual(httpValue, exactValue, 'RelTol', 1e-14)
+
+        end
+    end
+end
