2bfcf4d60ccb38707fdacd3ed8addd0761ed3bf9
Felix Benning <felix.benning@gmail.com>
2023-05-05 17:32:06 +0200

    test serving

--- python/tests/autoumbridge_test.py
+++ python/tests/autoumbridge_test.py
@@ -1,8 +1,13 @@
 from umbridge.autoumbridge import autoUm
+from umbridge import serve_models
 
 def test_wrapper():
     @autoUm(use_jax=True)
     def f(x):
         x**2
+    
+    serve_models([f])
 
-    f(2)
+
+if __name__ == "__main__":
+    test_wrapper()
