6222b6cd3bfd74a0f4887c3bd0e9c0750bd0aede
Sergey Dolgov <sergey.v.dolgov@gmail.com>
2023-11-13 16:08:25 +0000

    TT-Toolbox client readme

--- clients/matlab/ttClient.m
+++ clients/matlab/ttClient.m
@@ -2,16 +2,15 @@
 % Check for (and download) TT Toolbox
 check_tt;
 
-uri = 'http://localhost:4243';
-model = HTTPModel(uri, 'posterior');
+model = HTTPModel('http://localhost:4243', 'posterior', 'webwrite');
 
 % TT demo with umbridge model
-tol = 1e-5;
 d = model.get_input_sizes
 x = linspace(-5,5,33); % A uniform grid on [-5,5]^d for analytic-banana
-% TTlogLikelihood = amen_cross(numel(x)*ones(d,1), @(i)model.evaluate(x(i)), tol, 'vec', false)
+tol = 1e-5;
+% TTlogPosterior = amen_cross(repmat(numel(x),d,1), @(i)model.evaluate(x(i)), tol, 'vec', false)
 tic;
-TTlogLikelihood = greedy2_cross(numel(x)*ones(d,1), @(i)model.evaluate(x(i)), tol, 'vec', false)
+TTlogPosterior = greedy2_cross(repmat(numel(x),d,1), @(i)model.evaluate(x(i)), tol, 'vec', false)
 toc
 
 % benchmark-analytic-banana: 44.138067 seconds with 'send' engine
