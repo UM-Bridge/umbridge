075b2acd74db03c8979f6adea4bf0e5fbcd6b6c4
Lev Gromov <gromovl3a@gmail.com>
2023-12-20 17:38:04 +0100

    Remove unused functions related to SLURM jobs.

--- hpc/LoadBalancer.hpp
+++ hpc/LoadBalancer.hpp
@@ -29,34 +29,6 @@ std::string getCommandOutput(const std::string command)
     return output;
 }
 
-// state = ["PENDING","RUNNING","COMPLETED","FAILED","CANCELLED"]
-bool waitForJobState(const std::string &job_id, const std::string &state = "COMPLETED")
-{
-    const std::string command = "scontrol show job " + job_id + " | grep -oP '(?<=JobState=)[^ ]+'";
-    // std::cout << "Checking runtime: " << command << std::endl;
-    std::string job_status;
-
-    do
-    {
-        job_status = getCommandOutput(command);
-
-        // Delete the line break
-        if (!job_status.empty())
-            job_status.pop_back();
-
-        // Don't wait if there is an error or the job is ended
-        if (job_status == "" || (state != "COMPLETE" && job_status == "COMPLETED") || job_status == "FAILED" || job_status == "CANCELLED")
-        {
-            std::cerr << "Wait for job status failure, status : " << job_status << std::endl;
-            return false;
-        }
-        // std::cout<<"Job status: "<<job_status<<std::endl;
-        sleep(1);
-    } while (job_status != state);
-
-    return true;
-}
-
 // Check for every 100 ms, wait for maximum 20 second
 bool waitForFile(const std::string &filename, int time_out = 20)
 {
@@ -75,40 +47,6 @@ bool waitForFile(const std::string &filename, int time_out = 20)
     }
 
     return true;
-}
-
-// Start a slurm job and return job id
-std::string submitJob(const std::string &command)
-{
-    std::string sbatch_command = command + " | awk '{print $4}'"; // extract job ID from sbatch output
-    std::cout << "Submitting job with command: " << command << std::endl;
-
-    std::string job_id;
-    int i = 0;
-    do
-    {
-        job_id = getCommandOutput(sbatch_command);
-
-        // Delete the line break
-        if (!job_id.empty())
-            job_id.pop_back();
-
-        std::cout << "job_id: " << job_id << std::endl;
-        ++i;
-
-    } while (waitForJobState(job_id, "RUNNING") == false && i < 3 && waitForFile("./urls/url-" + job_id + ".txt", 20) == false);
-    // Wait to start all nodes on the cluster, call scontrol for every 1 sceond to check
-    // Also wait until job is running and url file is written
-    // Try maximum 3 times
-
-    // Check if the job is running
-    if (waitForJobState(job_id, "RUNNING") == false || waitForFile("./urls/url-" + job_id + ".txt", 10) == false)
-    {
-        std::cout << "Submit job failure." << std::endl;
-        exit(-1);
-    }
-
-    return job_id;
 }
 
 std::string readUrl(const std::string &filename)
@@ -133,47 +71,6 @@ std::string readUrl(const std::string &filename)
 
     return url;
 }
-
-class SingleSlurmJob
-{
-public:
-    SingleSlurmJob(std::string model_name = "forward")
-    {
-        // start a SLURM job for single request
-        job_id = submitJob("sbatch model.slurm");
-
-        const std::string server_url = readUrl("./urls/url-" + job_id + ".txt"); // read server url from txt file
-        // May use $SLURM_LOCALID in a .slurm file later
-
-        std::cout << "Hosting sub-server at : " << server_url << std::endl;
-
-        // List supported models
-        std::vector<std::string> models = umbridge::SupportedModels(server_url);
-        std::cout << "Supported models: " << std::endl;
-        for (auto model : models)
-        {
-            std::cout << "  " << model << std::endl;
-        }
-        std::cout << "Using model: " << model_name << std::endl;
-
-        // Start a client, using unique pointer
-        client_ptr = std::make_unique<umbridge::HTTPModel>(server_url, model_name); // use the first model avaliable on server by default
-    }
-
-    ~SingleSlurmJob()
-    {
-        // Cancel the SLURM job
-        std::system(("scancel " + job_id).c_str());
-
-        // Delete the url text file
-        std::system(("rm ./urls/url-" + job_id + ".txt").c_str());
-    }
-
-    std::unique_ptr<umbridge::HTTPModel> client_ptr;
-
-private:
-    std::string job_id;
-};
 
 // state = ["WAITING", "RUNNING", "FINISHED", "CANCELED"]
 bool waitForHQJobState(const std::string &job_id, const std::string &state = "COMPLETED")
