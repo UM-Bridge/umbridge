c3f7e621ce28c52598ec222a0835064f2a60370d
chun9l <97897047+chun9l@users.noreply.github.com>
2024-05-22 15:08:34 +0100

    Chane input conversion and added desc to input value help

--- testing/protocol_conformity_1.0/conftest.py
+++ testing/protocol_conformity_1.0/conftest.py
@@ -1,10 +1,8 @@
 # Pass --model_url on to individual tests
 
-import json
-
 def pytest_addoption(parser):
     parser.addoption("--model_url", action="store", help="what model URL to connect to")
-    parser.addoption("--input_value", action="store", default=0.0, help="value of constant input vector to be passed to the model, since some models may not handle the default well")
+    parser.addoption("--input_value", action="store", default=0.0, help="input vector or constant value to fill the vector to be passed to the model, since some models may not handle the default well. No spaces allowed when inputting vector!")
 
 def pytest_generate_tests(metafunc):
     option_value = metafunc.config.option.model_url
@@ -14,11 +12,7 @@ def pytest_generate_tests(metafunc):
     option_value = metafunc.config.option.input_value
     if 'input_value' in metafunc.fixturenames and option_value is not None:
         if isinstance(option_value, str):
-            option_value = json.loads(option_value)
-            if isinstance(option_value, list):
-                option_value = list(map(float, option_value))
-                metafunc.parametrize("input_value", [option_value])
-            else:
-                metafunc.parametrize("input_value", [float(option_value)])
+            option_value = eval(option_value)
+            metafunc.parametrize("input_value", [option_value])
         else:
             metafunc.parametrize("input_value", [float(option_value)])
